1e3ffe7c257aa4252bab961b7502e2584675d3bc
Sthefany Evangelista Matias <117083316+sthefanyematias@users.noreply.github.com>
2026-01-21 06:23:37 -0300

    Update 02.monitoramento.sh

--- 02.scripts/02.monitoramento.sh
+++ 02.scripts/02.monitoramento.sh
@@ -1,12 +1,23 @@
+# shebang - define que o script deve ser interpretado pelo bash
 #!/bin/bash
 
-echo "Uso de CPU:"
+# exibe o titulo da seção de cpu
+echo "--- uso de cpu ---"
+# o comando top em modo batch (-b) com 1 atualização (-n1) pegando as 5 primeiras linhas
 top -bn1 | head -5
 
+# pula uma linha para melhorar a leitura
 echo ""
-echo "Memória:"
+
+# exibe o titulo da seção de memoria
+echo "--- memoria ram ---"
+# exibe o uso de memória em formato human readable (leitura humana)
 free -h
 
+# pula uma linha
 echo ""
-echo "Disco:"
+
+# exibe o titulo da seção de armazenamento
+echo "--- uso de disco ---"
+# exibe o uso das partições em formato human readable
 df -h
