c60fe6a8bb625e17a0524740864453b32a36eef8
Sthefany Evangelista Matias <117083316+sthefanyematias@users.noreply.github.com>
2026-01-21 06:26:11 -0300

    Update 03.limpeza.sh

--- 02.scripts/03.limpeza.sh
+++ 02.scripts/03.limpeza.sh
@@ -1,6 +1,22 @@
+# shebang - define que o script deve ser interpretado pelo bash
 #!/bin/bash
 
-echo "Removendo arquivos temporários..."
-rm -rf /tmp/*
+# exibe mensagem de inicio da limpeza
+echo "--- iniciando limpeza do sistema ---"
 
-echo "Limpeza concluída."
+# remove arquivos temporários (o sudo á necessário para limpar certas pastas)
+# o asterisco (*) indica para apagar o conteúdo, mas manter a pasta /tmp
+echo "removendo arquivos temporarios em /tmp..."
+sudo rm -rf /tmp/*
+
+# limpa o cache de pacotes do gerenciador apt (economiza muito espaço)
+echo "limpando cache do gerenciador de pacotes (apt)..."
+sudo apt clean
+
+# remove pacotes que foram instalados como dependências e não são mais usados
+echo "removendo dependencias desnecessarias..."
+sudo apt autoremove -y
+
+# exibe mensagem de finalização
+echo ""
+echo "limpeza concluida com sucesso."
